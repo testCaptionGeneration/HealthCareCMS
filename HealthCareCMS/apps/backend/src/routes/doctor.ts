d60994e5aa13a4774b42660c20f9cb1b06ec5016
Rohit Rode <roderohit071@gmail.com>
2025-02-09 21:47:47 +0530

    doctor.ts

--- HealthCareCMS/apps/backend/src/routes/doctor.ts
+++ HealthCareCMS/apps/backend/src/routes/doctor.ts
@@ -2,7 +2,10 @@ import express from "express";
 import { doctorSignup, doctorSignin } from "../controller/doctorController";
 import { validateRequest } from "../Middleware/ValidateRequest";
 import { doctorSignupSchema, signinSchema } from "../../../shared/validation"
+
 const doctorrouter = express.Router();
+
 doctorrouter.post("/signup", validateRequest(doctorSignupSchema), doctorSignup);
 doctorrouter.post("/signin", validateRequest(signinSchema), doctorSignin);
+
 export default doctorrouter;
