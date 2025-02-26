070376e9ebde44c8075571b13e5f1133ccfff9d9
manan <daxini.manan@gmail.com>
2025-02-26 18:16:07 +0530

    bug fix

--- HealthCareCMS/apps/frontend/src/pages/Prescription.tsx
+++ HealthCareCMS/apps/frontend/src/pages/Prescription.tsx
@@ -10,7 +10,6 @@ import { useMedicines } from "../hooks/useMedicines";
 import { CloseIcon } from "../Icons/CloseIcon";
 import { Loader } from "../Components/LoaderSckelton";
 import { AddDiseasePopup } from "../Components/Inputs/AddDiseasePopup";
-import { NavbarComponent } from "../Components/NavbarComponent";
 
 interface Patient {
     fullName: string;
@@ -35,8 +34,7 @@ export const PrescriptionComponent = () => {
     const [loader, setLoader] = useState(true);
     const { prescriptionId } = useParams<{ prescriptionId: string }>();
     const [refresh, setRefresh] = useState(true);
-    const [content, setContent] = useState<string>("");
-    const [doctorId,setDoctorId]=useState("");
+    const [, setContent] = useState<string>("");
 
     useEffect(() => {
         console.log(prescriptionId);
@@ -57,13 +55,6 @@ export const PrescriptionComponent = () => {
         };
 
         fetchDetails();
-
-        const fetchDoctorId=async ()=>{
-            const response=await axios.get(`${BACKEND_URL}cms/v1/doctor/prescription/${prescriptionId}`);
-
-            setDoctorId(response.data.response.doctorId);
-        }
-        fetchDoctorId();
         const handleMedicineUpdate = () => setRefresh((prev) => !prev);
         window.addEventListener("medicineUpdated", handleMedicineUpdate);
         return () => window.removeEventListener("medicineUpdated", handleMedicineUpdate);
@@ -84,8 +75,7 @@ export const PrescriptionComponent = () => {
     });
 
     return (
-        <div>
-        <NavbarComponent DoctorId={doctorId}/>
+        <PageWrapper>
             <div className="flex justify-center items-start mt-5 px-4">
                 <div className="relative w-[1350px] h-auto p-4 rounded-lg border border-gray-300 shadow-lg bg-white">
                     <CreatePrescription open={prescriptionMedication} setOpen={setPrescriptionMedication} />
@@ -113,7 +103,6 @@ export const PrescriptionComponent = () => {
                             variant="secondary"
                             title="Add Disease"
                             onClick={() => setDiseasePopupOpen(true)}
-                            className="hover:bg-[#3B9AB8] hover:text-white transition duration-300"
                         />
                     </div>
 
@@ -127,7 +116,6 @@ export const PrescriptionComponent = () => {
                                 variant="secondary"
                                 title="Add Medicine"
                                 onClick={() => setPrescriptionMedication(true)}
-                                className="hover:bg-[#3B9AB8] hover:text-white transition duration-300"
                             />
                         </div>
 
@@ -145,7 +133,8 @@ export const PrescriptionComponent = () => {
                                 {isLoading ? (
                                     <Loader className="mx-auto my-7" />
                                 ) : medication?.length > 0 ? (
-                                    medication.map((med, index) => (
+                                    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                                    medication.map((med:any, index) => (
                                         <div
                                             key={index}
                                             className="grid grid-cols-6 text-center p-3 text-gray-600 border-b odd:bg-gray-50 even:bg-white"
@@ -159,10 +148,9 @@ export const PrescriptionComponent = () => {
                                                 <Button
                                                     title="Delete"
                                                     startIcon={<CloseIcon size={28.85} />}
-                                                    variant="danger"
+                                                    variant="secondary"
                                                     size="md"
                                                     onClick={() => deleteMedicine(med._id)}
-                                                    className="hover:bg-red-600 hover:text-white transition duration-300"
                                                 />
                                             </div>
                                         </div>
@@ -187,17 +175,13 @@ export const PrescriptionComponent = () => {
 
                     {/* Submit Button */}
                     <div className="flex justify-center p-3">
-                        <Button variant="primary" title="Submit" size="md" onClick={async () =>{ alert("Prescription submitted!");
-                        await axios.post(`${BACKEND_URL}cms/v1/doctor/treatmentcontent`,{
-                            prescriptionId,
-                            content
-                        })
+                        <Button variant="primary" title="Submit" size="md" onClick={() =>{ alert("Prescription submitted!");
                             window.history.go(-1);
                         }
                         } />
                     </div>
                 </div>
             </div>
-            </div>
+        </PageWrapper>
     );
 };
